8428ed586f70c8d89e5609a3f687b99ebaae12fb
yaocs2 <yaocs2@midea.com>
2021-01-03 16:04:24 +0800

    nginx-url-hash> 资源初始化

---
+++ src/main/java/imooc/WarStarterApplication.java
@@ -0,0 +1,11 @@
+package imooc;
+
+// 打包war [4] 增加war的启动类
+//public class WarStarterApplication extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        // 指向Application这个springboot启动类
+//        return builder.sources(Application.class);
+//    }
+//}
